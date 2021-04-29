9e7787e86e16ba49645ed385f473e4730f90a3d1
PedroHenriqueDevBR <p.lyma18@gmail.com>
2021-04-29 20:53:44 -0300

    feat: init first level

---
+++ game/Actor.gd
@@ -0,0 +1,7 @@
+extends KinematicBody2D # KinematicBody serve como um corpo que interage com o ambiente
+class_name Actor # define uma classe para que possa ser extendida com o nome Actor
+
+export var speed: = Vector2(300.0, 1000.0) # Velocidade do jogador
+export var gravity: = 4000.0 # Gravidade aplicada a queda
+var _velocity: = Vector2.ZERO # Velocidade inicial (Controle da posição do player)
+const FLOOR_NORMAL: = Vector2.UP # Serve para informar a engine que e um jogo de plataforma e o chão fica abaixo
