4a91145737a8880f89a91bb29ad952b41ccdddde
PedroHeriqueDevBR <p.lyma18@gmail.com>
2021-05-04 23:21:35 -0300

    feat: implements player movements

--- game/src/Actors/Player.gd
+++ game/src/Actors/Player.gd
@@ -1,9 +1,7 @@
 extends Actor
 
-export var stomp_impulse: = 1000.0
-
-func _on_EnemyDetector_area_entered(area) -> void:
-	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
+const  MAX_JUMP: = 1
+var jump_count = 0
 
 
 func _on_EnemyDetector_body_entered(body) -> void:
@@ -17,13 +15,14 @@ func _physics_process(delta) -> void:
 	var is_jump_interupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interupted)
+	set_animation(direction)
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
 	
 func get_direction() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("move_right") - 
 		Input.get_action_strength("move_left"),
-		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0
+		-1.0 if Input.is_action_just_pressed("jump") else 0.0
 	)
 
 func calculate_move_velocity(
@@ -35,17 +34,28 @@ func calculate_move_velocity(
 		var out: = linear_velocity
 		out.x = speed.x * direction.x
 		out.y += gravity * get_physics_process_delta_time()
-		if direction.y == -1.0:
+		if direction.y == -1.0 and jump_count < MAX_JUMP:
 			out.y = speed.y * direction.y
-		if is_jump_interupted:
-			out.y = 0.0
+			jump_count += 1
+		if is_on_floor():
+			jump_count = 0
 		return out
-
-
-func calculate_stomp_velocity(
-		linear_velocity: Vector2, 
-		impulse: float
-	) -> Vector2:
-		var out: = linear_velocity
-		out.y = -impulse
-		return out
+		
+		
+func set_animation(direction: Vector2) -> void:
+	if not is_on_floor():
+		$AnimatedSprite.play('jump')
+		if direction.x < 0:
+			$AnimatedSprite.flip_h = true
+		elif direction.x > 0:
+			$AnimatedSprite.flip_h = false
+	else:
+		if direction.x < 0:
+			$AnimatedSprite.play('run')
+			$AnimatedSprite.flip_h = true
+		elif direction.x > 0:
+			$AnimatedSprite.play('run')
+			$AnimatedSprite.flip_h = false
+		else:
+			$AnimatedSprite.play('idle')
+	
